53dec4ffd1595399122eb649d363702430fe1e7a
79201180913-svg <79201180913@mail.ru>
2026-08-20 18:54:09 +0300

    feat: enable API operation history page

--- src/edward/ui/gui_launcher.py
+++ src/edward/ui/gui_launcher.py
@@ -7,12 +7,14 @@ from edward.ui.error_reporting import install_error_reporting
 from edward.ui.trading_ui_guard import install_trading_ui_guard
 from edward.ui.sandbox_funding_ui import install_sandbox_funding_ui
 from edward.ui.sandbox_data_routing import install_sandbox_data_routing
+from edward.ui.operations_history_ui import install_operations_history_ui
 
 
 install_sandbox_data_routing()
 install_error_reporting(EdwardApp)
 install_trading_ui_guard(EdwardApp)
 install_sandbox_funding_ui(EdwardApp)
+install_operations_history_ui(EdwardApp)
 
 
 if __name__ == "__main__":
