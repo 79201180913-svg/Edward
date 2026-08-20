56e60dcdcf3d0adea5b997f9328151f116ba72c9
79201180913-svg <79201180913@mail.ru>
2026-08-20 14:17:53 +0300

    feat: expose application services

---
+++ src/edward/services/__init__.py
@@ -0,0 +1,13 @@
+from .account_service import AccountService
+from .balance_service import BalanceService
+from .instrument_service import InstrumentService
+from .market_service import MarketService
+from .portfolio_service import PortfolioService
+
+__all__ = [
+    "AccountService",
+    "BalanceService",
+    "InstrumentService",
+    "MarketService",
+    "PortfolioService",
+]
