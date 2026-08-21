fb580d1870333dd5d1970fc7b773ad28c292783d
79201180913-svg <79201180913@mail.ru>
2026-08-21 18:10:41 +0300

    fix: install compatibility fixes for instrument and history data

--- src/edward/ui/gui_launcher.py
+++ src/edward/ui/gui_launcher.py
@@ -9,6 +9,7 @@ from edward.ui.sandbox_funding_ui import install_sandbox_funding_ui
 from edward.ui.sandbox_data_routing import install_sandbox_data_routing
 from edward.ui.operations_history_ui import install_operations_history_ui
 from edward.ui.ux_fixes import install_ux_fixes
+from edward.ui.compat_fixes import install_compat_fixes
 
 
 install_sandbox_data_routing()
@@ -17,6 +18,7 @@ install_trading_ui_guard(EdwardApp)
 install_sandbox_funding_ui(EdwardApp)
 install_operations_history_ui(EdwardApp)
 install_ux_fixes(EdwardApp)
+install_compat_fixes(EdwardApp)
 
 
 if __name__ == "__main__":
