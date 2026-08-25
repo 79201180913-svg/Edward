2b13bc6e06326b641d1859e00eeb292f342c01ba
79201180913-svg <79201180913@mail.ru>
2026-08-25 18:10:35 +0300

    Wire v0.4 Opportunity Search UI

--- src/edward/ui/gui_launcher.py
+++ src/edward/ui/gui_launcher.py
@@ -41,6 +41,7 @@ from edward.ui.settings_page_v04 import install_settings_page
 from edward.ui.analysis_ui_v04 import install_analysis_ui
 from edward.ui.decision_context_ui_v04 import install_decision_context_ui
 from edward.ui.analysis_ui_consistency_v04 import install_analysis_ui_consistency
+from edward.ui.opportunity_search_ui_v04 import install_opportunity_search_ui
 from edward.config.settings import Environment
 
 
@@ -95,6 +96,7 @@ install_settings_page(EdwardApp)
 install_analysis_ui(EdwardApp, TInvestAdapterClient)
 install_analysis_ui_consistency()
 install_decision_context_ui(EdwardApp)
+install_opportunity_search_ui(EdwardApp)
 
 
 if __name__ == "__main__":
