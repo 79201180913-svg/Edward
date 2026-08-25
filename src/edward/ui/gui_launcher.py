2f1315222464c866611f3a6226bc7d796463677b
79201180913-svg <79201180913@mail.ru>
2026-08-25 18:05:54 +0300

    Install v0.4 analysis strategy consistency UI patch

--- src/edward/ui/gui_launcher.py
+++ src/edward/ui/gui_launcher.py
@@ -40,6 +40,7 @@ from edward.ui.orders_page_v03_ticker_fix import install as install_orders_ticke
 from edward.ui.settings_page_v04 import install_settings_page
 from edward.ui.analysis_ui_v04 import install_analysis_ui
 from edward.ui.decision_context_ui_v04 import install_decision_context_ui
+from edward.ui.analysis_ui_consistency_v04 import install_analysis_ui_consistency
 from edward.config.settings import Environment
 
 
@@ -92,6 +93,7 @@ install_orders_page_v03(EdwardApp)
 install_orders_ticker_fix(TInvestAdapterClient)
 install_settings_page(EdwardApp)
 install_analysis_ui(EdwardApp, TInvestAdapterClient)
+install_analysis_ui_consistency()
 install_decision_context_ui(EdwardApp)
 
 
